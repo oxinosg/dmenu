40c35e27678ae535f916b1e95b35e6453b245a8a
oxinosg <oxinosg@gmail.com>
2020-04-05 15:57:53 +0200

    init

--- config.def.h
+++ config.def.h
@@ -2,10 +2,10 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-/* -fn option overrides fonts[0]; default X11 font or font set */
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
+/* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-  "SourceCodePro:style=Regular:size=14"
+	"monospace:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
@@ -15,8 +15,7 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines = 10;
-/* static const char width[] = "#222222"; */
+static unsigned int lines      = 0;
 
 /*
  * Characters not considered part of a word while deleting words
